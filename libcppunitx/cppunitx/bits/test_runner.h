c30cf66344995e78c135706859a6f65b513505d2
Kaz Nishimura <kazssym@vx68k.org>
2018-04-25 08:53:52 +0900

    Add new files

---
+++ libcppunitx/cppunitx/bits/test_runner.h
@@ -0,0 +1,30 @@
+// cppunitx/bits/test_runner.h
+// Copyright (C) 2018 Kaz Nishimura
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _CPPUNITX_BITS_TEST_RUNNER_H
+#define _CPPUNITX_BITS_TEST_RUNNER_H 1
+
+namespace cppunitx
+{
+    class test_runner
+    {
+    public:
+    };
+}
+
+#endif
