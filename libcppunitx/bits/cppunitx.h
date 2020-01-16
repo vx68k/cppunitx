2cbd5a7583060c101fd49e55a1d05ac3590cb4a8
Kaz Nishimura <kazssym@linuxfront.com>
2020-01-16 20:32:52 +0900

    Add new header

---
+++ libcppunitx/bits/cppunitx.h
@@ -0,0 +1,42 @@
+// <bits/cppunitx.h>
+// Copyright (C) 2018-2020 Kaz Nishimura
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _CPPUNITX_H
+#define _CPPUNITX_H 1
+
+#ifndef _CPPUNITX_PUBLIC
+#if _WIN32
+#if CPPUNITX_DLL
+#define _CPPUNITX_PUBLIC __declspec(dllexport)
+#else
+#define _CPPUNITX_PUBLIC __declspec(dllimport)
+#endif
+#else /* !_WIN32 */
+#if defined __has_attribute
+#if __has_attribute(visibility)
+#define _CPPUNITX_PUBLIC __attribute__((visibility("default")))
+#endif
+#endif /* defined __has_attribute */
+#endif /* !_WIN32 */
+#endif
+
+#ifndef _CPPUNITX_PUBLIC
+#define _CPPUNITX_PUBLIC
+#endif
+
+#endif
