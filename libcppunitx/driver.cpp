3ae5c08d21d740b1dfb504a02b7ac926f52a8763
Kaz Nishimura <kazssym@linuxfront.com>
2020-01-21 13:06:43 +0900

    Add code to load modules

--- libcppunitx/driver.cpp
+++ libcppunitx/driver.cpp
@@ -25,17 +25,64 @@
 
 #include <stdexcept>
 #include <cstdio>
+#include <ltdl.h>
+
+namespace
+{
+    /// Helper class for 'lt_dlinit' and 'lt_dlexit'.
+    struct libltdl
+    {
+        libltdl()
+        {
+            int result = lt_dlinit();
+            if (result != 0) {
+                throw std::runtime_error("'lt_dlinit' failed");
+            }
+        }
+
+        ~libltdl()
+        {
+            int result = lt_dlexit();
+            if (result != 0) {
+                // Destructors cannot throw exceptions.
+                std::fprintf(stderr, "'lt_dlexit' failed\n");
+            }
+        }
+    };
+
+    /// Helper class for 'lt_dlopen' and 'lt_dlclose'.
+    struct module
+    {
+        lt_dlhandle handle;
+
+        explicit module(const char *const name)
+            : handle(lt_dlopen(name))
+        {
+            if (handle == nullptr) {
+                throw std::runtime_error("'lt_dlopen' failed");
+            }
+        }
+
+        ~module()
+        {
+            int result = lt_dlclose(handle);
+            if (result != 0) {
+                // Destructors cannot throw exceptions.
+                std::fprintf(stderr, "'lt_dlclose' failed\n");
+            }
+        }
+    };
+}
 
 using std::exception;
+using std::logic_error;
+using std::runtime_error;
 using std::shared_ptr;
 using std::fprintf;
 using namespace cppunitx;
 
-namespace
-{
-    const int SKIP = 77;
-    const int ERROR = 99;
-}
+const int SKIP = 77;
+const int ERROR = 99;
 
 // Class 'TestDriver' implementation.
 
@@ -48,6 +95,7 @@ shared_ptr<TestDriver> TestDriver::getInstance()
 int TestDriver::main(const int argc, char **const argv)
 {
     try {
+        libltdl lib;
         auto driver = getInstance();
         for (int i = 1; i != argc; i += 1) {
             driver->run(argv[i]);
@@ -70,5 +118,6 @@ TestDriver::~TestDriver()
 
 void TestDriver::run(const char *const suiteName)
 {
-    throw std::logic_error("Not implemented");
+    module opened(suiteName);
+    throw logic_error("Not implemented");
 }
