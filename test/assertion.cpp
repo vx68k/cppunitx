23e744044e80ca3c6b7e4a1ab9ab6463c4cf67d4
Kaz Nishimura <kazssym@linuxfront.com>
2020-01-31 12:50:13 +0900

    Add a new test suite

---
+++ test/assertion.cpp
@@ -0,0 +1,53 @@
+// assertion.cpp
+// Copyright (C) 2020 Kaz Nishimura
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#define SUITE assertion
+
+#define MODULE_MAIN 1
+#include <cppunitx/module>
+
+#include <cppunitx/framework>
+#include <cppunitx/assert>
+
+using namespace cppunitx;
+using namespace cppunitx::assertion;
+
+class AssertionTest
+{
+private:
+    Before setUp {[this]() {
+        }};
+
+    After tearDown {[this]() {
+        }};
+
+    Test testAssertNull {"test 'assertNull'", [this]() {
+            assertNull(0);
+            // TODO: Add more assertions here.
+        }};
+
+    Test testAssertNotNull {"test 'assertNotNull'", [this]() {
+            assertNotNull(this);
+            // TODO: Add more assertions here.
+        }};
+};
+TestRegistrant<AssertionTest> test {"assertion"};
