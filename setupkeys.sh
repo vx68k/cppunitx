cfa7dbf64b29980972e1816a5e0ec597cd264e37
Kaz Nishimura <kazssym@linuxfront.com>
2020-02-25 21:51:32 +0900

    Move key setup code into a script

---
+++ setupkeys.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export GNUPGHOME=${GNUPGHOME:-$HOME/.gnupg}
+
+mkdir -p -m 0700 "$GNUPGHOME"
+echo allow-preset-passphrase >> "$GNUPGHOME"/gpg-agent.conf || exit $?
+
+gpg --import --batch "$1" || exit $?
+
+keygrip=`gpg --list-secret-keys --with-colons "$GPG_USERNAME" | \
+    awk -F : '$1 == "grp" { print $10 }'`
+exec /usr/lib/gnupg/gpg-preset-passphrase --preset "$keygrip"
