4a2e4884000cfda427e559ab87d63e156069af75
Kaz Nishimura <kazssym@linuxfront.com>
2020-02-26 08:18:39 +0900

    Add licensing comments

--- setupkeys.sh
+++ setupkeys.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# setupkeys.sh - set up keys for signing
+# Copyright (C) 2020 Kaz Nishimura
+#
+# Copying and distribution of this file, with or without modification, are
+# permitted in any medium without royalty provided the copyright notice and
+# this notice are preserved.  This file is offered as-is, without any warranty.
 
 export GNUPGHOME=${GNUPGHOME:-$HOME/.gnupg}
 
