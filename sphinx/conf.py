c84d9142a220c35085fa35e3b53c2ba44424d794
Kaz Nishimura <kazssym@linuxfront.com>
2020-05-07 18:41:18 +0900

    Change the release to indicate the DRAFT status

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -26,7 +26,7 @@ author = 'Kaz Nishimura'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0-DRAFT'
 
 
 # -- General configuration ---------------------------------------------------
