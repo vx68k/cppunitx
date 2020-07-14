598adde35aeeeb0d440296f251a31d9ab2cc4dca
Kaz Nishimura <kazssym@linuxfront.com>
2020-07-14 14:21:53 +0900

    Update the release number

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -26,7 +26,7 @@ author = 'Kaz Nishimura'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0-DRAFT'
+release = '1-ALPHA'
 
 
 # -- General configuration ---------------------------------------------------
